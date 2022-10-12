4cb0dd2a4e1a89e31e94db421a90768a83d0fe64
Krystof Woldrich <krystofwoldrich@gmail.com>
2022-10-12 12:16:51 +0200

    Add sample app

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import {useState} from "react";
+import type {Node} from 'react';
+import {
+SafeAreaView,
+StatusBar,
+Text,
+Button,
+} from 'react-native';
+import Calculator from 'new-arch-example/src/index'
+const App: () => Node = () => {
+const [currentResult, setResult] = useState<number | null>(null);
+return (
+    <SafeAreaView>
+    <StatusBar barStyle={'dark-content'}/>
+    <Text style={{marginLeft:20, marginTop:20}}>3+7={currentResult ?? "??"}</Text>
+    <Button title="Compute" onPress={async () => {
+        const result = await Calculator.add(3, 7);
+        setResult(result);
+    }} />
+    </SafeAreaView>
+);
+};
+export default App;
